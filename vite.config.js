6c9fc7acb9b80617156847ff62552f49cd3661f0
MonaRose1 <maimoonanaumani2@gmail.com>
2026-02-16 19:54:34 +0500

    Config: Set base path to root '/'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages repository name is 'Nex', so we need this base path for production
-  base: process.env.NODE_ENV === 'production' ? "/Nex/" : "/",
+  base: "/",
 })
