a7b16f8fa1574c8a1646c224db534251daacd13e
MonaRose1 <maimoonanaumani2@gmail.com>
2026-02-16 19:48:20 +0500

    Fix: specific config for Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Nex/",
+  // GitHub Pages repository name is 'Nex', so we need this base path for production
+  base: process.env.NODE_ENV === 'production' ? "/Nex/" : "/",
 })
